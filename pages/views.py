1352f4c67322ba6d76754055a5b8c3de5ebf90cc
FireWallDragonDarkFluid <79576662+FireWallDragonDarkFluid@users.noreply.github.com>
2021-05-21 12:56:08 +0800

    Initial commit

---
+++ pages/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from listings.models import Listing
+from listings.choices import price_choices,bedroom_choices,state_choices
+
+def index(request):
+    listings = Listing.objects.all()[:3]
+    context = {
+        'listings':listings,
+        'state_choices':state_choices,
+        'bedroom_choices':bedroom_choices,
+        'price_choices':price_choices
+    }
+    return render(request,'pages/index.html',context)
+def about(request):
+    return render(request,'pages/about.html')
